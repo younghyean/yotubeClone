18c2b391697c78a5c3de2e76789473d9adb5e51b
yonghyen <ankim2son@gmail.com>
2021-03-01 15:04:48 +0900

    업로드 FORM 생성 & 비디오파일 저장

---
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -0,0 +1,132 @@
+import React, { useState } from 'react'
+import {Typography, Button, Form, message, Input, Icon} from 'antd';
+import Dropzone from 'react-dropzone';
+import Axios from 'axios';
+
+const {TextArea} = Input;
+const {Title} = Typography;
+
+const PrivateOption = [
+    {value : 0, label : "Private" },
+    {value : 1, label : "Public" }
+]
+const CategoryOption = [
+    {value : 0, label : "Film & Animation" },
+    {value : 1, label : "Autos & Vehicles" },
+    {value : 2, label : "Music" },
+    {value : 3, label : "Pets & Animals" },
+]
+
+function VideoUploadPage() {
+
+    const [VideoTitle, setVideoTitle] = useState("")
+    const [Description, setDescription] = useState("")
+    const [Private, setPrivate] = useState(0)
+    const [Category, setCategory] = useState("Film & Animation")
+
+    const onTitleChange =(e) => {
+        setVideoTitle(e.currentTarget.value);
+    }
+    const onDescriptionChange =(e) => {
+        setDescription(e.currentTarget.value);
+    }
+    const onPrivateChange =(e) => {
+        setPrivate(e.currentTarget.value);
+    }
+    const onCategoryChange =(e) => {
+        setCategory(e.currentTarget.value);
+    }
+    const onDrop = (files) => {
+
+        let formData = new FormData;
+        const config = {
+            header : { 'content-type' : 'multipart/form-data'}
+        }
+        formData.append("file", files[0]);
+        
+        Axios.post('/api/video/uploadfiles', formData, config)
+        .then(response => {
+            if(response.data.success){
+                console.log(response.data);
+            }else{
+                alert('업로드 실패');
+            }
+        })
+
+    }
+    return (
+        <div style = {{ maxWidth : '700PX', margin : '2rem auto'}}>
+            <div style = {{ textAlign :'center', marginBottom : '2rem'}}>
+                <Title level={2}>Upload Video</Title>
+            </div>
+
+            <Form onSubmit>
+                <div style ={{ display :'flex', justifyContent:'space-between'}}>
+                    {/* drom */}
+
+                    <Dropzone 
+                    onDrop = {onDrop}
+                    multiple = {false}
+                    maxSize = {1000000000}
+                    >
+
+                    {({ getRootProps, getInputProps}) =>(
+                        <div style = {{ width : '300px', height : '240px', border : '1px solid lightgray', display :'flex', 
+                        alignItems : 'center', justifyContent : 'center'}} {...getRootProps()}>
+                            <input {...getInputProps()} ></input>
+                            <Icon type ="plus" style = {{ fontSize : '3rem'}}></Icon>
+                        </div>
+                        
+                    )}
+                    </Dropzone>
+                    { /* thumbnail*/}
+                    <div>
+                        <img src alt />
+                    </div>
+                </div>
+              
+                <br />
+                <br />
+
+                <label>Title</label>
+                <Input
+                    onChange={onTitleChange}
+                    value = {VideoTitle}
+                />
+                <br />
+                <br />
+                <label>Description</label>
+                <TextArea
+                    onChange={onDescriptionChange}
+                    value ={Description}
+                />
+                <br />
+                <br />
+
+                <select onChange = {onPrivateChange}>
+
+                    {PrivateOption.map((item, index) => (
+                         <option key ={index} value = {item.value}>{item.label}</option>
+                    ))}
+                </select>
+
+                <br />
+                <br />
+
+                <select onChange = {onCategoryChange}>
+
+                {CategoryOption.map((item, index) => (
+                <option key ={index} value = {item.value}>{item.label}</option>
+                ))}
+                </select>
+
+                <br />
+                <br />
+
+                <Button type ="primary" size ="large" onClick>Submit</Button>
+            </Form>
+        </div>
+    )
+}
+
+export default VideoUploadPage
